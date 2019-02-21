64d3fae01cb58f56cccdd7eb7f6c7fd795d6abdc
xojan0120 <xojan0120@gmail.com>
2019-02-21 13:22:53 +0900

    [Fix] テストコード修正

--- test.rb
+++ test.rb
@@ -1,15 +1,5 @@
 
-p User.all
-p UserRoom.all
-p Room.all
-p DirectMessage.all
-p DirectMessageStat.all
-
-puts "#"*10
-puts "destroy!"
-puts "#"*10
-
-User.second.destroy
+#User.second.destroy
 #UserRoom.first.destroy
 #Room.first.destroy
 #DirectMessage.first.destroy
